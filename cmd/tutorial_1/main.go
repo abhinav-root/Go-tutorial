32a8b68b4923da4bb946a7deacd72fac8c22988f
abhinav <abhinavsinghkkr@gmail.com>
2024-03-10 14:47:56 +0530

    Variable basics

--- cmd/tutorial_1/main.go
+++ cmd/tutorial_1/main.go
@@ -4,4 +4,20 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello go")
+	var age int = 18
+	// we can omit the type
+	var age2 = 23
+	// we can also omit the var syntax
+	age3 := 32
+	var weight float32 = 74.23
+	var myname string = "Abhinav Singh"
+	var isMarried bool = false
+	isMarried = true
+
+	fmt.Println(age, age2, age3, weight, myname, isMarried)
+
+	// constants never change
+	// we cannot use short syntax when declaring constants.
+	const moneyInRupees int = 73000
+	fmt.Println(moneyInRupees)
 }
---
+++ variable_basics/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var age int = 31
+	age = 23
+	fmt.Println("age is: ", age)
+
+	var name = "Abhinav Singh"
+	fmt.Println("Your name is ", name)
+
+	car, cost := "Audi", 50000
+	fmt.Println(car, cost)
+
+	var a, b, c int
+	fmt.Println(a, b, c)
+}
